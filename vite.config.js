b16f94379ef3cbad0ef1faec7fe5b05469a6a8bd
Szymon Podstufka (prywatne) <szymonpodstufka@iMac-Szymon.kopnet.pl>
2022-12-22 22:10:54 +0100

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/coffee-recipes-app/",
+  base: "/coffee-recipes/",
   plugins: [vue()],
   resolve: {
     alias: {
